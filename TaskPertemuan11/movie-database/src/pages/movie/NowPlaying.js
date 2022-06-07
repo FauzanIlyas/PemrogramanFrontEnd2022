a3e926813972112e552c803e23a023a9b287d08d
Fauzan Ilyas <fauz19073ti@student.nurulfikri.ac.id>
2022-06-07 22:26:41 +0700

    ubah folder dan file TaskPertemuan11

--- TaskPertemuan11/movie-database/src/pages/movie/NowPlaying.js
+++ TaskPertemuan11/movie-database/src/pages/movie/NowPlaying.js
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movies from "../../components/Movies/Movies";
+import Hero from "../../components/Hero/Hero";
+
 function NowPlayingMovie() {
+  // Simpan API_KEY dan URL ke variable
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
+
+  // Membuat state movies
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getNowPlayingMovies();
+    // // Fetch data dari axios
+    // const response = await axios(URL);
+    // //console.log(response.data.results);
+
+    // // Simpan data ke state
+    // setMovies(response.data.results);
+  }, []);
+
+  async function getNowPlayingMovies() {
+    // Fetch data dari axios
+    const response = await axios(URL);
+    //console.log(response.data.results);
+
+    // Simpan data ke state
+    setMovies(response.data.results);
+  }
+
+  console.log(movies);
+
   return (
     <>
-      <h2>Now Playing Movie</h2>
+      <Hero />
+      <Movies title="NOW PLAYING" movies={movies} />
     </>
   );
 }
 
 export default NowPlayingMovie;
+
+// function NowPlayingMovie() {
+//   return (
+//     <>
+//       <h2>Now Playing Movie</h2>
+//     </>
+//   );
+// }
+
+// export default NowPlayingMovie;
--- TaskPertemuan11/movie-database/src/pages/movie/TopRated.js
+++ TaskPertemuan11/movie-database/src/pages/movie/TopRated.js
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movies from "../../components/Movies/Movies";
+import Hero from "../../components/Hero/Hero";
+
 function TopRatedMovie() {
+  // Simpan API_KEY dan URL ke variable
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
+
+  // Membuat state movies
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getTopRatedMovies();
+    // // Fetch data dari axios
+    // const response = await axios(URL);
+    // //console.log(response.data.results);
+
+    // // Simpan data ke state
+    // setMovies(response.data.results);
+  }, []);
+
+  async function getTopRatedMovies() {
+    // Fetch data dari axios
+    const response = await axios(URL);
+    //console.log(response.data.results);
+
+    // Simpan data ke state
+    setMovies(response.data.results);
+  }
+
+  console.log(movies);
+
   return (
     <>
-      <h2>Top Rated</h2>
+      <Hero />
+      <Movies title="TOP RATED" movies={movies} />
     </>
   );
 }
 
 export default TopRatedMovie;
+
+// function NowPlayingMovie() {
+//   return (
+//     <>
+//       <h2>Now Playing Movie</h2>
+//     </>
+//   );
+// }
+
+// export default NowPlayingMovie;
+
+
+
+// function TopRatedMovie() {
+//   return (
+//     <>
+//       <h2>Top Rated</h2>
+//     </>
+//   );
+// }
+
+// export default TopRatedMovie;
